ec2f0b70a2d4796334e7d11d7e8e6c5b482e6afc
Muhammad Noman Shaukat <maxdigital245@gmail.com>
2026-08-21 15:19:45 +0500

    SEO & Template Refinement: resolved duplicate H1 markup tags by replacing breadcrumbs heading with styled div tag

--- resources/views/frontend/components/breadcrumbs.blade.php
+++ resources/views/frontend/components/breadcrumbs.blade.php
@@ -17,7 +17,7 @@
 
 <div class="global-breadcrumb-banner text-center">
     <div class="container">
-        <h1 class="fw-bold mb-3 h2">{{ $title }}</h1>
+        <div class="fw-bold mb-3 h2 text-white">{{ $title }}</div>
         <nav class="fw-semibold">
             <a href="/">Home</a> <span class="mx-2">/</span> 
             @if(isset($parent) && isset($parent_link))
